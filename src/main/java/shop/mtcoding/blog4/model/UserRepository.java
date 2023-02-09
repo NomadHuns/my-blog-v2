6ad0c201daa51b8577107eebf2db259faaa6ab94
kimalss22 <kimalss22@gmail.com>
2023-02-09 12:18:31 +0900

    회원가입 매퍼 작성 및 리파지토리 구현 + 서비스 기능 구현

---
+++ src/main/java/shop/mtcoding/blog4/model/UserRepository.java
@@ -0,0 +1,26 @@
+package shop.mtcoding.blog4.model;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserRepository {
+    public List<User> findAll();
+
+    public User findById(int id);
+
+    public int insert(@Param("username") String username, @Param("password") String password,
+            @Param("email") String email);
+
+    public int deleteById(int id);
+
+    public int updateById(@Param("id") int id, @Param("username") String username,
+            @Param("password") String password,
+            @Param("email") String email);
+
+    public User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+
+    public User findByUsername(String username);
+}
