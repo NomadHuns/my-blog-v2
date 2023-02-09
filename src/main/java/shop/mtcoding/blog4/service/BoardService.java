ca94a61a106618347caa32de8187d5c338446f9d
kimalss22 <kimalss22@gmail.com>
2023-02-09 16:04:44 +0900

    게시물 목록 불러오기 기능 Controller 및 Service 구현

--- src/main/java/shop/mtcoding/blog4/service/BoardService.java
+++ src/main/java/shop/mtcoding/blog4/service/BoardService.java
@@ -1,11 +1,14 @@
 package shop.mtcoding.blog4.service;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import shop.mtcoding.blog4.dto.board.BoardReq.SaveReqDto;
+import shop.mtcoding.blog4.dto.board.BoardResp.BoardListRespDto;
 import shop.mtcoding.blog4.ex.CustomException;
 import shop.mtcoding.blog4.model.BoardRepository;
 
@@ -22,6 +25,11 @@ public class BoardService {
         } catch (Exception e) {
             throw new CustomException("글쓰기 실패", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public List<BoardListRespDto> getBoardList() {
+        List<BoardListRespDto> boardDtoList = boardRepository.findAllWithUser();
+        return boardDtoList;
     }
 
 }
