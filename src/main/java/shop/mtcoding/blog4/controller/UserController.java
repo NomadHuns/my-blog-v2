4903ae1ae278549751bb0c97fabf1d7100fa2c37
NomadHuns <118786401+NomadHuns@users.noreply.github.com>
2023-02-09 12:27:32 +0900

    feat: 회원가입 기능 구현 완료 (#4)
    * 회원가입 컨트롤러 기능 구현
    
    * 회원가입 테스트 코드 작성
    
    * 회원가입 매퍼 작성 및 리파지토리 구현 + 서비스 기능 구현

--- src/main/java/shop/mtcoding/blog4/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog4/controller/UserController.java
@@ -2,9 +2,18 @@ package shop.mtcoding.blog4.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.RequiredArgsConstructor;
+import shop.mtcoding.blog4.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.blog4.ex.CustomException;
+import shop.mtcoding.blog4.service.UserService;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
     @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
@@ -18,5 +27,20 @@ public class UserController {
     @GetMapping("/user/updateForm")
     public String updateForm() {
         return "user/updateForm";
+    }
+
+    @PostMapping("/join")
+    public String join(JoinReqDto joinReqDto) {
+        validateString(joinReqDto.getUsername(), "유저네임을 입력하세요");
+        validateString(joinReqDto.getPassword(), "패스워드를 입력하세요");
+        validateString(joinReqDto.getEmail(), "이메일을 입력하세요");
+        userService.join(joinReqDto);
+        return "redirect:/loginForm";
+    }
+
+    private void validateString(String stringData, String msg) {
+        if (stringData.isEmpty() || stringData == null) {
+            throw new CustomException(msg);
+        }
     }
 }
---
+++ src/main/java/shop/mtcoding/blog4/service/UserService.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.blog4.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import shop.mtcoding.blog4.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.blog4.ex.CustomException;
+import shop.mtcoding.blog4.model.User;
+import shop.mtcoding.blog4.model.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void join(JoinReqDto joinReqDto) {
+        checkAlreadyHasSameUsername(joinReqDto.getUsername());
+        try {
+            userRepository.insert(joinReqDto.getUsername(), joinReqDto.getPassword(), joinReqDto.getEmail());
+        } catch (Exception e) {
+            throw new CustomException("회원가입 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void checkAlreadyHasSameUsername(String username) {
+        User principalPS = userRepository.findByUsername(username);
+        if (principalPS != null) {
+            throw new CustomException("존재하는 유저네임입니다.");
+        }
+    }
+}
