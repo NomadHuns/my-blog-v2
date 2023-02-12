f768bb5dbb85816b273c11e6b38d35c0120a4992
kimalss22 <kimalss22@gmail.com>
2023-02-13 07:56:52 +0900

    댓글 삭제 기능 Controller 및 Service 구현

--- src/main/java/shop/mtcoding/blog4/controller/ReplyController.java
+++ src/main/java/shop/mtcoding/blog4/controller/ReplyController.java
@@ -3,10 +3,13 @@ package shop.mtcoding.blog4.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.blog4.dto.ResponseDto;
 import shop.mtcoding.blog4.model.User;
 import shop.mtcoding.blog4.service.ReplyService;
 import shop.mtcoding.blog4.util.Verify;
@@ -22,5 +25,12 @@ public class ReplyController {
         User principal = Verify.authenticatePrincipal(session);
         replyService.save(comment, id, principal.getId());
         return "redirect:/board/" + id;
+    }
+
+    @DeleteMapping("/reply/{id}")
+    public @ResponseBody ResponseDto<?> delete(@PathVariable("id") Integer id) {
+        User principal = Verify.authenticatePrincipalApi(session);
+        replyService.delete(id, principal.getId());
+        return new ResponseDto<>(1, "댓글 삭제 성공", null);
     }
 }
